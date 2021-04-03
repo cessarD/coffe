62886ce509cc038d99e41693c6323b0b40df5025
Karlyn <00106718@gmail.com>
2021-04-03 17:38:29 -0600

    Cambio de imagen en el Header

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,7 +5,7 @@ export default function Header() {
 
 //TODO cambiar el SRC
     return <>
-      <img src={`../../imagenes/headbanner.png`} style={{width:"100%",height:"10vh"}}/>
+      <img src={`../../imagenes/headbanner.png`} style={{width:"20%",height:"15vh"}}/>
     </>
 
 
