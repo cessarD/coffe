c620ff8193af79cfca13b41ef5ae2b6ade308b50
Karlyn <00106718@gmail.com>
2021-04-03 18:42:10 -0600

    Imagenes Carousel

--- src/component/Slider.js
+++ src/component/Slider.js
@@ -15,17 +15,31 @@ class Slider extends Component {
                 transitionTime="500"
                 >
         <div>
-          <img src="http://www.fillmurray.com/400/300" />
+          <img src="https://i.pinimg.com/564x/fa/02/cf/fa02cf1fa878a200b2726b89070f2a03.jpg" />
 
         </div>
         <div>
-          <img src="http://www.fillmurray.com/400/300" />
+          <img src="https://i.pinimg.com/564x/e1/28/77/e1287702f9cfdb83028bf7a03eb05e35.jpg" />
 
         </div>
         <div>
-          <img src="http://www.fillmurray.com/400/200" />
+          <img src="https://i.pinimg.com/originals/ab/c7/1d/abc71d367c8e88a20e21a0b1e9723f96.jpg" />
 
         </div>
+        <div>
+          <img src="https://i.pinimg.com/originals/3b/46/b9/3b46b9cb63be130d8f3dfb177a382f4c.jpg" />
+
+        </div>
+        <div>
+          <img src="https://i.pinimg.com/564x/64/7e/64/647e647326067f8f6bc2810cdb64c801.jpg" />
+
+        </div>
+        <div>
+          <img src="https://i.pinimg.com/564x/ce/7f/16/ce7f1646f40164235244d33711cf35eb.jpg" />
+
+        </div>
+
+
       </Carousel>
       </div>
     );
